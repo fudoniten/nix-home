94f5f2560a40b167ce368812c6b883bd7c6df5c1
niten <niten@fudo.org>
2021-09-30 17:54:04 -0700

    added module.nix

---
+++ module.nix
@@ -0,0 +1,37 @@
+{ doom-emacs, niten-doom-config, ... }:
+
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  user-configs = {
+    niten = ./niten.nix;
+    root = ./niten.nix;
+    viator = ./niten.nix;
+    xiaoxuan = ./xiaoxuan.nix;
+  };
+
+  hostname = config.instance.hostname;
+  enable-gui = config.fudo.hosts.${hostname}.enable-gui;
+
+in {
+
+  config.home-manager = {
+    useGlobalPkgs = true;
+
+    users = let
+      generate-config = username: config-file: let
+        user-cfg = config.fudo.users.${username};
+        user-email = user-cfg.email;
+        home-dir = user-cfg.home-directory;
+      in import user-configs.${username}
+        { inherit username user-email home-dir; };
+    in mapAttrs generate-config {
+      niten = ./niten.nix;
+      # FIXME: Root shouldn't have all this stuff installed!
+      root = ./niten.nix;
+      viator = ./niten.nix;
+      xiaoxuan = ./xiaoxuan.nix;
+    };
+  };
+}
