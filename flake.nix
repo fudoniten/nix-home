2a88b452108d329f74f90fe75fa0186e77da37ac
niten <niten@fudo.org>
2021-12-19 07:20:52 -0800

    Don't have a nixpkgs input for home-manager to follow

--- flake.nix
+++ flake.nix
@@ -2,10 +2,7 @@
   description = "Fudo Home Manager Configuration";
 
   inputs = {
-    home-manager = {
-      url = "github:nix-community/home-manager/release-21.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
     doom-emacs.url = "github:vlaci/nix-doom-emacs";
     niten-doom-config = {
       url = "git+https://git.fudo.org/niten/doom-emacs.git";
