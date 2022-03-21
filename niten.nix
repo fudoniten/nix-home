b37bac74bb7e4f19c5735c4112a8a707885a8724
niten <niten@fudo.org>
2022-03-21 14:24:26 -0700

    [kitty] Spaces in the font name

--- niten.nix
+++ niten.nix
@@ -150,7 +150,7 @@ in {
       font = {
         package = pkgs.nerdfonts;
         # name = "Terminess (TTF) Nerd Font Complete Mono";
-        name = "ShureTechMono Nerd Font Complete Mono";
+        name = "Shure Tech Mono Nerd Font Complete Mono";
         size = 14;
         #package = pkgs.inconsolata;
         #name = "Incosolata";
