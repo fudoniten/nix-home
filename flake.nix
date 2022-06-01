225e0a66a47c1cc2e374d15a35af16b955f8fb08
niten <niten@fudo.org>
2022-06-01 13:57:19 -0700

    Switch to 22.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Fudo Home Manager Configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-21.11";
+    nixpkgs.url = "nixpkgs/nixos-22.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-21.11";
+      url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     niten-doom-config = {
