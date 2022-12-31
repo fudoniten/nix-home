e3cdafad99023733e0aa8a94627746574b101fcd
niten <niten@fudo.org>
2022-12-31 10:08:28 -0800

    Add pytorch to python

--- users/niten.nix
+++ users/niten.nix
@@ -20,7 +20,7 @@ let
   };
 
   python-with-packages =
-    pkgs.python3.withPackages (pyPkgs: with pyPkgs; [ opencv4 ]);
+    pkgs.python3.withPackages (pyPkgs: with pyPkgs; [ opencv4 torch ]);
 
   emacs-packages = with pkgs.emacsPackages; [
     elpher
