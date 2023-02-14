96d4cbe8eb915518a6b98a1b8ae32b37cd1af824
niten <niten@fudo.org>
2023-02-13 17:12:07 -0800

    Use toplevel intead of config

--- module.nix
+++ module.nix
@@ -52,34 +52,24 @@ in {
     };
   };
 
-  config.home-manager = let
-    allUsers = attrNames config.users.users;
-    users = filter (username: hasAttr username user-map) allUsers;
+  config.home-manager = let users = attrNames toplevel.config.users.users;
   in {
-    users =
-      (genAttrs users (username: { config.programs.bash.enable = true; }));
+    users = (genAttrs users (username:
+      mkIf (hasAttr username user-map) (let
+        userOpts = config.users.users."${username}";
+        configUser = user-map."${username}";
+        configFile = ./. + "/users/${configUser}.nix";
+      in import config-file inputs {
+        inherit lib pkgs;
+        inherit (userOpts) username user-email home-dir;
+        inherit (cfg) enable-gui enable-kitty-term;
+      }))) // {
+        root = import ./users/root.nix inputs {
+          inherit pkgs lib;
+          username = "root";
+          user-email = "root@${cfg.local-domain}";
+          home-dir = "/root";
+        };
+      };
   };
-
-  # config.home-manager = let users = attrNames config.users.users;
-  # in {
-  #   useGlobalPkgs = true;
-
-  #   users = (genAttrs users (username:
-  #     mkIf (hasAttr username user-map) (let
-  #       userOpts = config.users.users."${username}";
-  #       configUser = user-map."${username}";
-  #       configFile = ./. + "/users/${configUser}.nix";
-  #     in import config-file inputs {
-  #       inherit lib pkgs;
-  #       inherit (userOpts) username user-email home-dir;
-  #       inherit (cfg) enable-gui enable-kitty-term;
-  #     }))) // {
-  #       root = import ./users/root.nix inputs {
-  #         inherit pkgs lib;
-  #         username = "root";
-  #         user-email = "root@${cfg.local-domain}";
-  #         home-dir = "/root";
-  #       };
-  #     };
-  # };
 }
