47172d429cce307cb7e9500c784fddf4bb0168eb
niten <niten@fudo.org>
2024-04-28 11:38:49 -0700

    Enable hyprland for niten

--- users/niten.nix
+++ users/niten.nix
@@ -228,6 +228,12 @@ in {
 
     # gnome-manager.background = ./static/k3gy64wu8i5a1.png;
 
+    wayland.windowManager.hyprland = {
+      enable = true;
+      xwayland.enable = true;
+      systemd.enable = true;
+    };
+
     programs = {
       bash = {
         enable = true;
